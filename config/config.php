645da043f5e6a922dd7c5c3dabefea6dc93c6d9a
zjf0618 <zjf814907217@163.com>
2019-08-02 11:52:57 +0800

    修改admin登录关闭多单位2

--- config/config.php
+++ config/config.php
@@ -91,12 +91,16 @@ define('VERSION', $config['version']);
 define('HIGHPASS', $config['highpass']);
 define('SYSURL', ''.URL.PATH.'.php');
 define('UPURL', $config['upurl']);
-define('ISMORECOM', $config['companymode']);
-//if($_COOKIE['xinhu_ca_adminuser'] == 'admin'){
-//    define('ISMORECOM', false);
-//}else{
-//    define('ISMORECOM', $config['companymode']);
-//}
+if(!empty($_COOKIE['xinhu_ca_adminuser'])){
+    if($_COOKIE['xinhu_ca_adminuser'] == 'admin'){
+        define('ISMORECOM', false);
+    }else{
+        define('ISMORECOM', $config['companymode']);
+    }
+}else{
+    define('ISMORECOM', $config['companymode']);
+}
+
 
 
 if(file_exists($_confpath))include_once($_confpath);
